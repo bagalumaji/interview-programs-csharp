a63b177791c1f2f5c1efb491d9a0a15b8230e9dd
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-02-09 12:21:59 +0530

    Add project files.

---
+++ interview-programs-csharp/strings/SubStringInStrings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interview_programs_csharp.strings
+{
+    internal class SubStringInStrings
+    {
+        public static String FindSbStringInString(String[] str)
+        {
+            string s1 = "";
+            string s2 = "";
+            string s = str[0];
+            string[] words = str[1].Split(',');
+            foreach(string word in words)
+            {
+                int l = word.Length;
+                if (s.StartsWith(word) && s1.Length < l)
+                {
+                    s1 = word;
+                }else if (s.EndsWith(word) && s2.Length < l)
+                {
+                    s2 = word;
+                }
+            }
+
+            return s1+","+ s2;
+        }
+        public static void Main(String[] args)
+        {
+            String[] str = { "baseball", "base,a,all,ball" };
+            Console.WriteLine(FindSbStringInString(str));
+
+        }
+    }
+   
+}
