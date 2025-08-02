b1f83635e36cdddf73ccfdbca439b3a65f1a9d14
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-02 16:46:41 +0530

    created demo class

---
+++ interview-programs-csharp/Demo.cs
@@ -0,0 +1,20 @@
+﻿using interview_programs_csharp.arrays;
+
+namespace interview_programs_csharp
+{
+    public class Demo
+    {
+        public static void Main(string[] args)
+        {
+            //int[] arr = { 3, 1, 2, 3, 4, 2, 4, 5 };
+            //arr = SortAndRemoveDuplicates.SortArray(arr);
+            //int l = SortAndRemoveDuplicates.RemoveDuplicates(arr);
+
+            //for (int i = 0; i < l; i++)
+            //{
+            //    Console.Write(arr[i] + " ");
+            //}
+
+        }
+    }
+}
