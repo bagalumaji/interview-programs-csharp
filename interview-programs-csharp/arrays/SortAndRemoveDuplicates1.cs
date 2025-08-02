b188329a3b3582ae973a4a718369965e6b907b3f
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-02 16:47:13 +0530

    added another way to sort and remove duplicate

---
+++ interview-programs-csharp/arrays/SortAndRemoveDuplicates1.cs
@@ -0,0 +1,38 @@
+﻿namespace interview_programs_csharp.arrays
+{
+    public class SortAndRemoveDuplicates1
+    {
+        public static int[] SortArray(int[] arr)
+        {
+           for(int i=0;i<arr.Length;i++)
+            {
+                for (int j=0;j<arr.Length;j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        int t = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = t;
+                    }
+                }
+            }
+            return arr;
+        }   
+
+        public static int RemoveDuplicates(int[] arr)
+        {
+            int i = 0, j = 1;
+            while (j<arr.Length)
+            {
+                if (arr[i] != arr[j])
+                {
+                    i++;
+                    arr[i] = arr[j];
+                }
+                j++;
+            }
+            return i + 1;
+        }
+       
+    }
+}
