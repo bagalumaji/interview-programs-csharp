47c817d44386fa04c59093c069aab3fa222057ab
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-02 17:27:11 +0530

    created  a comparator for sort for salary

---
+++ interview-programs-csharp/comparer/SortEmployeeByName.cs
@@ -0,0 +1,13 @@
+﻿using interview_programs_csharp.poco;
+
+namespace interview_programs_csharp.comparer
+{
+    public class SortEmployeeByName : Comparer<Employee>
+    {
+        public override int Compare(Employee x, Employee y)
+        {
+           return x.Name.CompareTo(y.Name);
+        }
+    }
+
+}
