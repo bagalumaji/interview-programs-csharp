6c5fd8e510fa4acb78f10d95dbbbb67106ab6a18
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-02 16:55:23 +0530

    added logic for sort and remove duplicates

--- interview-programs-csharp/Demo.cs
+++ interview-programs-csharp/Demo.cs
@@ -6,7 +6,14 @@ namespace interview_programs_csharp
     {
         public static void Main(string[] args)
         {
-            //int[] arr = { 3, 1, 2, 3, 4, 2, 4, 5 };
+            int[] arr = { 3, 1, 2, 3, 4,6, 2, 4, 5 };
+            int[] result = SortAndRemoveDuplicates1.SortArrayAndRemoveDuplicates(arr);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
             //arr = SortAndRemoveDuplicates.SortArray(arr);
             //int l = SortAndRemoveDuplicates.RemoveDuplicates(arr);
 
--- interview-programs-csharp/arrays/SortAndRemoveDuplicates1.cs
+++ interview-programs-csharp/arrays/SortAndRemoveDuplicates1.cs
@@ -2,37 +2,11 @@
 {
     public class SortAndRemoveDuplicates1
     {
-        public static int[] SortArray(int[] arr)
+        public static int[] SortArrayAndRemoveDuplicates(int[] arr)
         {
-           for(int i=0;i<arr.Length;i++)
-            {
-                for (int j=0;j<arr.Length;j++)
-                {
-                    if (arr[i] < arr[j])
-                    {
-                        int t = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = t;
-                    }
-                }
-            }
-            return arr;
-        }   
-
-        public static int RemoveDuplicates(int[] arr)
-        {
-            int i = 0, j = 1;
-            while (j<arr.Length)
-            {
-                if (arr[i] != arr[j])
-                {
-                    i++;
-                    arr[i] = arr[j];
-                }
-                j++;
-            }
-            return i + 1;
+            Array.Sort(arr);
+            //arr.Distinct().ToArray();
+            return [.. arr.Distinct()];
         }
-       
     }
 }
