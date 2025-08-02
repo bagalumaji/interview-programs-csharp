56290333f2a413c31618d1414ac22baf146f4b94
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-02 17:26:44 +0530

    created poco for employee details

---
+++ interview-programs-csharp/poco/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace interview_programs_csharp.poco
+{
+    public class Employee
+    {
+
+        public string Name { get; }
+        public int Salary { get; }
+        public Employee(string name, int salary)
+        {
+
+            Name = name;
+
+            Salary = salary;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Salary: {Salary}";
+        }
+    }
+}
