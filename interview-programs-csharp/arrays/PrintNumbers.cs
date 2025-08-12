5108175e5e29eea15e4ee0b24dda942f9b75512d
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-12 20:51:37 +0530

    print numbers

---
+++ interview-programs-csharp/arrays/PrintNumbers.cs
@@ -0,0 +1,16 @@
+﻿namespace interview_programs_csharp.arrays
+{
+    public class PrintNumbers
+    {
+        public static void PrintNumbersInRange(int[]arr)
+        {
+            for (int i = 2; i < arr.Length; i++)
+            {
+                if (arr[i-2]+1 == arr[i-1] && arr[i-1]+1 == arr[i])
+                {
+                    Console.WriteLine(arr[i-2] + " " + arr[i-1]+" " + arr[i]);
+                }
+            }
+        }
+    }
+}
