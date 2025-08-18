efe7b65efc7d4dfb9944b5c12650c87c744e46dc
DESKTOP-2HHA8QU\umaji bagal <bagalumaji@gmail.com>
2025-08-18 18:43:25 +0530

    pairs of sums

---
+++ interview-programs-csharp/arrays/PairsOfSums.cs
@@ -0,0 +1,20 @@
+﻿namespace interview_programs_csharp.arrays
+{
+    public class PairsOfSums
+    {
+        public static int[] PairOfSumFromArray(int[]arr, int target)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == target)
+                    {
+                        return new int[] { arr[i], arr[j] };
+                    }
+                }
+            }
+            return new int[] { };
+        }
+    }
+}
