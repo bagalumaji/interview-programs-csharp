e0220233d721d697541dff1536edafef976044c4
DESKTOP-2HHA8QU\umaji bagal <bagalumaji@gmail.com>
2025-08-19 14:58:45 +0530

    all pairs of sums

---
+++ interview-programs-csharp/arrays/AllPairsOfSums.cs
@@ -0,0 +1,20 @@
+﻿namespace interview_programs_csharp.arrays
+{
+    public class AllPairsOfSums
+    {
+        public static Dictionary<int,int> AllSumPairs(int[]arr,int target)
+        {
+            var pairs = new Dictionary<int, int>();
+            for(int i=0;i<arr.Length;i++)
+            {
+                var val = target - arr[i];
+                if(!pairs.ContainsKey(val) && !pairs.ContainsValue(val))
+                {
+                    pairs.Add(arr[i], val);
+                }
+            }
+            Console.WriteLine(pairs);
+            return pairs;
+        }
+    }
+}
